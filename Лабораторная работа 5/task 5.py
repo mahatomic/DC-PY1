df582646a0c651a4d58157a56eeb41f17abfa4a8
Maria <115428286+mahatomic@users.noreply.github.com>
2022-11-13 00:48:13 +0300

    Update task 5.py
    Исправила название переменной

--- Лабораторная работа 5/task 5.py
+++ Лабораторная работа 5/task 5.py
@@ -2,9 +2,9 @@ from random import sample
 import string
 
 
-def get_random_password(n=8) -> str:
+def get_random_password(size=8) -> str:
     alphabet = string.ascii_uppercase + string.digits + string.ascii_lowercase
-    password = "".join(sample(alphabet, n))
+    password = "".join(sample(alphabet, size))
     return password
 
 
